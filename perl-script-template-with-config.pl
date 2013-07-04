519484936d9e84dac3111aa530ea97d2a7c859f2
Fedor Sumkin <qosys.net@gmail.com>
2013-07-04 10:25:30 +0400

    script with config

--- perl-script-template-with-config.pl
+++ perl-script-template-with-config.pl
@@ -22,7 +22,7 @@ use Getopt::Long qw ( GetOptions );
 use 5.008003; # may be newer
 ########################################################################
 my $VERSION = '0.8.0';
-my $CONFIG_FILE = 'config.cfg'
+my $CONFIG_FILE = 'config.cfg';
 
 ## Set defaults for all the options, then read them in from command line
 my %arg = (
@@ -32,8 +32,13 @@ my %arg = (
     help => 0,
     mode => 0
 );
-
 ########################################################################
+# Get our configuration information
+if (my $err = ReadCfg($CONFIG_FILE)) {
+    print(STDERR $err, "\n");
+    exit(1);
+}
+
 my $result = GetOptions (
    \%arg,
    'verbose',
@@ -48,6 +53,11 @@ $arg{help} and help();
 print "================\n";
 print "ARG ARRAY => ".Dumper \%arg;
 print "================\n";
+
+print "================\n";
+print "CFG ARRAY => ".Dumper \%CFG::CFG;
+print "================\n";
+
 ########################################################################
 
 sub main
@@ -57,7 +67,35 @@ sub main
         exit 0;
     }
 
+}
 
+########################################################################
+# Read a configuration file
+#   The arg can be a relative or full path, or
+#   it can be a file located somewhere in @INC.
+sub ReadCfg
+{
+    my $file = $_[0];
+
+    our $err;
+
+    {   # Put config data into a separate namespace
+        package CFG;
+
+        # Process the contents of the config file
+        my $rc = do($file);
+
+        # Check for errors
+        if ($@) {
+            $::err = "ERROR: Failure compiling '$file' - $@";
+        } elsif (! defined($rc)) {
+            $::err = "ERROR: Failure reading '$file' - $!";
+        } elsif (! $rc) {
+            $::err = "ERROR: Failure processing '$file'";
+        }
+    }
+
+    return ($err);
 }
 
 ########################################################################
